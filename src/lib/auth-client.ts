9dbfe19e6e99b9229979a174b8f472579a0307e2
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-07-14 22:18:01 +0600

    fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-   baseURL:process.env.BETTER_AUTH_URL|| "http://localhost:3000",
+   // baseURL:process.env.BETTER_AUTH_URL|| "http://localhost:3000",
+   baseURL:"https://medicare-hub-client.vercel.app",
 })
 
 export const { signIn, signUp, useSession, getSession, signOut } = authClient
